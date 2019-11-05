ac0e02ef1bdfd88dfbd607b251c8aca11ba0e181
Amogh Bhagwat <amoghbhagwat00@gmail.com>
2019-11-05 23:41:58 +0530

    changes

--- convert.py
+++ convert.py
@@ -5,8 +5,7 @@ filename = "foo.npy"
 voxel_grid = np.load(filename)
 
 x_size, y_size, z_size = voxel_grid.shape
-points = np.empty((0,4), float)
-index = 0
+points = np.empty((0,3), float)
 voxel_grid[2][2][2] = 1
 voxel_size = 2
 x_min = -71.036
@@ -19,9 +18,9 @@ for i in range(x_size):
 				x = (voxel_size * i) + x_min
 				y = (voxel_size * j) + y_min
 				z = (voxel_size * k) + z_min
-				index = index + 1
-				point = [[index, x, y, z]]
+				# index = index + 1
+				point = [[x, y, z]]
 				points = np.r_[points, point]
-print(points)
+print(points.shape)
 # Save the numpy array for visualization
 np.save("voxel", points)
